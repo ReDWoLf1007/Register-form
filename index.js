6227d5be1c33adad211125e9bd44572c0e87195f
ReDWoLf1007 <sagar.tiwari.work@gmail.com>
2024-02-10 00:18:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require("express")      //Framwork - Simplifies making applications
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const dotenv = require("dotenv")        //Hides mongoDB password in database
+
+const app = express();
+dotenv.config();
+
+const port = process.env.PORT || 3000;
+
+mongoose.connect("mongodb://localhost/registrationDatabase");
+
+const registrationSchema = new mongoose.Schema({
+    name : String,
+    email : String,
+    password : String
+})
+
+//model of registration schema
+
+const Registration = mongoose.model("Registration", registrationSchema);        //database schema
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + "/pages/index.html");
+})
+
+app.post("/register", async(req, res)=>{
+    try{
+        const {name, email, password} = req.body;
+
+
+        const existingUser = await Registration.findOne({email : email});
+
+        //check for Existing user
+        if(!existingUser){
+
+            
+            const registrationData = new Registration({
+                name,
+                email,
+                password
+            });
+            await registrationData.save();
+            res.redirect("/success");
+        }
+        else{
+            console.log("User Already Exists")
+            res.redirect("/error");
+        }
+        }
+        catch(error){
+            res.redirect("error");
+    }
+})
+
+app.get("/success", (req, res)=>{
+    res.sendFile(__dirname + "/pages/success.html");
+})
+
+
+app.get("/error", (req,res)=>{
+    res.sendFile(__dirname + "/pages/error.html");
+})
+
+
+
+
+
+app.listen(port, ()=>{
+    console.log(`server is running on port ${port}`);
+})
